5a6b2b5fa01637735a0e5a0daf7eea0d929e7eb7
alexharris <alexharris6@gmail.com>
2019-10-29 22:37:14 -0700

    more color picker stuff, basic layout

--- components/ColorPicker.vue
+++ components/ColorPicker.vue
@@ -1,16 +1,37 @@
 <template>
-    <div class="border-solid border-2 max-w-md border-gray-600">
-        <!-- <div @click="updateColors">Color Picker</div> -->
-        <ul class="flex flex-wrap">
+<div class="m-5">
+    <div class="border-solid border shadow-lg rounded p-2 max-w-sm" >        
+        <ul class="flex flex-wrap" v-if="!showModal">
             <li v-for="(item,i) in palette">
-                <div class="w-10 h-10 m-1" v-bind:style="{backgroundColor: item['hex']}" @click="makeActiveSwatch(i)"></div>                
+                
+                <div 
+                    class="w-10 h-10 m-1 border border-gray-700 rounded-full cursor-pointer"
+                    v-bind:class="{'shadow-2xl': isActiveSwatch(i)}"
+                    v-bind:style="{backgroundColor: item['hex']}" 
+                    @click="makeActiveSwatch(i)" 
+                >
+                </div>                
             </li>
-            <li @click="addColor()">Add</li>
+            <li class="w-10 h-10 m-1 border-dashed border border-red-500 hover:border-gray-900 cursor-pointer rounded-full flex items-center justify-center font-bold text-4xl text-red-500 hover:text-gray-900" @click="addNewSwatch()">+</li>
         </ul>
-
-        <sketch-picker v-model="palette[activeSwatch]" @input="updatePallete" v-if="colorPickerOpen"/>
-
+        <div v-if="showModal">
+                <!-- <div class="inline-block w-full">
+                    <span class="rounded-full float-right font-bold text-gray-500" @click="showModal = !showModal">X</span>
+                </div> -->
+            <div class="color-modal">
+                <sketch-picker :value="palette[activeSwatch]"  @input="storeCurrentColor" />
+            </div>
+            <div class="mt-2">
+                <hr />
+                <div class="inline-block w-full mt-2">
+                    <button class="btn-grey float-left" @click="showModal = !showModal">Close</button>                     
+                    <button class="btn-blue float-right mr-2" @click="selectColor">Select</button>                     
+                    <button class="btn-red float-right mr-2" @click="removeSwatch(activeSwatch)">Remove</button>                    
+                </div>
+            </div>            
+        </div>
     </div>
+</div>
 </template>
 
 <script>
@@ -25,35 +46,73 @@ export default {
     data() {
         return {
             // numberOfColors: 4,
-            colorPickerOpen: false,
+            currentColor: '', // for temporary holding swatch value from color-picker
+            showModal: false,
             activeSwatch: 0,
             palette: [{hex: '#ff00ff'},{hex: '#ff0000'}], // the colors as they are created by the color pickers
             convertedColorArray: [], // the colors as they are sent to the ditherer ([0,0,0])
         }
     },
+    computed: {
+
+    },
     watch: { 
         initialPalette: function(newVal, oldVal) { // watch it
-            console.log('Prop changed: ', newVal, ' | was: ', oldVal)
+            // console.log('Prop changed: ', newVal, ' | was: ', oldVal)
             this.rgbToHex(newVal)
-        }
+        },
     },    
     methods: {
+        // When the user selects the color from the modal
+        selectColor() {
+            this.palette[this.activeSwatch] = {'hex': this.currentColor['hex']}
+            this.updatePallete()
+            this.showModal = false
+        },
+        // This holds the selected color until user hits Select in the color modal
+        storeCurrentColor(color) {
+            this.currentColor = color
+        },
+        // Determine if a swatch is the active one, this controls which classes display on that swatch when selected
+        isActiveSwatch(i) {
+            if(this.activeSwatch === i) {
+                return true
+            } else {
+                return false
+            }
+        } ,       
         // Make Active Swatch
         // When a swatch is clicked on, make it active and open the color picker
         makeActiveSwatch(i) {
             this.activeSwatch = i //tells the picker which swatch is being updated
-            this.colorPickerOpen = true
+            this.showModal = true
         },
+        // Add New Swatch
+        // This adds a new color to the colors        
+        addNewSwatch(){
+            this.palette.push({hex: '#ffffff'})
+            this.activeSwatch = this.palette.length
+            this.updatePallete()
+
+            
+        },
+        // Remove a swatch and update the palette      
+        removeSwatch(i) {
+            console.log(i)
+            this.palette.splice(i,1)
+            this.updatePallete()
+            this.showModal = false
+        },    
         // Update Colors
         // This updates the colors that are used by the actual ditherer
         // Requires an array of hex colors in the format [{hex: '#ff000'},{hex: '#ff0ff'}]
         updatePallete(){ // update the colors, requires an array of
             var colorTuplesArray = [];
             this.palette.forEach((v) => {
+                
                 colorTuplesArray.push(this.hexToRgb(v['hex']))
             })
             this.$emit('update-palette', colorTuplesArray)
-            this.colorPickerOpen = false
         },
         //Convert Hex to RGB
         // Given a hex value, return the RGB tuple
@@ -77,17 +136,38 @@ export default {
                 var hex = '#' + ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1);
                 this.palette.push({hex});  
             }
-        },  
-        // Add Color
-        // This adds a new color to the colors        
-        addColor(){
-            this.activeSwatch = this.activeSwatch + 1
-            this.palette.push({})
-            // console.log(this.colorPickerOpen)
-            this.colorPickerOpen = true
-            
-        }      
-     
+        },       
+    
     }
 }
 </script>
+
+<style>
+
+    .vc-sketch {
+    width: auto !important;
+    box-shadow: none !important;
+    }
+
+    .vc-sketch-alpha-wrap {
+        display: none;
+    }
+
+    .vc-sketch-field--single:last-of-type {
+        display: none;
+    }
+
+    .vc-sketch-presets {
+        display: none;
+    }
+    .vc-sketch-hue-wrap {
+        height: 24px !important;
+    }
+
+    .vc-hue-picker {
+        margin-top: 0 !important;
+        height: 24px !important;
+    }
+
+
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,18 +1,24 @@
 <template>
-  <div class="container max-w-full">
+  <div class="container mx-auto ">
     <h1 class="title">
       Dither it!
     </h1>  
-    <div class="flex">
-      <div class="flex-1">
+    <div class="flex flex-wrap">
+      <div class="w-full md:w-1/2 p-3">
         <ImageUpload v-on:image-upload="onImageUpload"  />  
       </div>
-      <div class="flex-1">
+      <div class="w-full md:w-1/2 p-3">
         <canvas id="ditheredImageCanvas" class="max-w-full" :width="ditheredCanvasWidth" :height="ditheredCanvasHeight"></canvas>
       </div>
     </div>
-    <ColorPicker v-on:update-palette="onUpdatePalette" :initial-palette="rgbQuantOptions.palette" />  
-    <div @click="ditherImage" class="inline-block bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Dither Image</div>
+    <div class="flex">
+      <div class="flex-1">
+        <ColorPicker v-on:update-palette="onUpdatePalette" :initial-palette="rgbQuantOptions.palette" />  
+      </div>
+      <div class="flex-1">
+        <button @click="ditherImage" class="btn-red">Dither Image</button>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -28,23 +34,23 @@ export default {
   },
   data() {
     return {
-      ditheredCanvasWidth: 500, // Width of the dithered canvas
-      ditheredCanvasHeight: 500, // Height of the dithered canvas
-      rgbQuantOptions: {
-          colors: 8,             // desired palette size
-          method: 1,               // histogram method, 2: min-population threshold within subregions; 1: global top-population
-          boxSize: [8,8],        // subregion dims (if method = 2)
-          boxPxls: 2,              // min-population threshold (if method = 2)
-          initColors: 4096,        // # of top-occurring colors to start with (if method = 1)
+      ditheredCanvasWidth: 500,       // Width of the dithered canvas
+      ditheredCanvasHeight: 500,      // Height of the dithered canvas
+      rgbQuantOptions: {              // ---- Options ------
+          colors: 8,                  // desired palette size
+          method: 1,                  // histogram method, 2: min-population threshold within subregions; 1: global top-population
+          boxSize: [8,8],             // subregion dims (if method = 2)
+          boxPxls: 2,                 // min-population threshold (if method = 2)
+          initColors: 4096,           // # of top-occurring colors to start with (if method = 1)
           minHueCols: 2000,           // # of colors per hue group to evaluate regardless of counts, to retain low-count hues
-          dithKern: 'FloydSteinberg',          // dithering kernel name, see available kernels in docs below
-          dithDelta: 0,            // dithering threshhold (0-1) e.g: 0.05 will not dither colors with <= 5% difference
-          dithSerp: false,         // enable serpentine pattern dithering
-          palette: [],             // a predefined palette to start with in r,g,b tuple format: [[r,g,b],[r,g,b]...]
-          reIndex: false,          // affects predefined palettes only. if true, allows compacting of sparsed palette once target palette size is reached. also enables palette sorting.
-          useCache: true,          // enables caching for perf usually, but can reduce perf in some cases, like pre-def palettes
-          cacheFreq: 10,           // min color occurance count needed to qualify for caching
-          colorDist: "euclidean",  // method used to determine color distance, can also be "manhattan"
+          dithKern: 'FloydSteinberg', // dithering kernel name, see available kernels in docs below
+          dithDelta: 0,               // dithering threshhold (0-1) e.g: 0.05 will not dither colors with <= 5% difference
+          dithSerp: false,            // enable serpentine pattern dithering
+          palette: [],                // a predefined palette to start with in r,g,b tuple format: [[r,g,b],[r,g,b]...]
+          reIndex: false,             // affects predefined palettes only. if true, allows compacting of sparsed palette once target palette size is reached. also enables palette sorting.
+          useCache: true,             // enables caching for perf usually, but can reduce perf in some cases, like pre-def palettes
+          cacheFreq: 10,              // min color occurance count needed to qualify for caching
+          colorDist: "euclidean",     // method used to determine color distance, can also be "manhattan"
       }
     }
   },
@@ -52,7 +58,7 @@ export default {
     // This receives a palette from ColorPicker in the form of an array of hex values
     onUpdatePalette(palette) {
       this.rgbQuantOptions.palette = []
-      this.rgbQuantOptions.palette.forEach((v,i) => {
+      palette.forEach((v,i) => {
         this.rgbQuantOptions.palette.push(v)
       })
     },
@@ -60,7 +66,7 @@ export default {
       this.ditheredCanvasWidth = imageCanvas.width
       this.ditheredCanvasHeight = imageCanvas.height    
       // console.log(this.imageCanvas)
-     this.analyzeImagePalette() 
+      this.analyzeImagePalette() 
       // // create new RgbQuant instance
       // var q = new RgbQuant(this.rgbQuantOptions);  
 
@@ -82,26 +88,13 @@ export default {
       
       // create new RgbQuant instance
       var q = new RgbQuant(this.rgbQuantOptions);  
-
-      // analyze histograms
-      q.sample(imageCanvas); 
-      
-      // build palette
-      if(this.rgbQuantOptions.palette == []) {
-        var pal = q.palette();
-      } else {
-        var pal = this.rgbQuantOptions.palette
-      }
-      
-      // reduce image
-      var ditheredImageData = q.reduce(imageCanvas)  
+      console.log(this.rgbQuantOptions.palette)
+      q.sample(imageCanvas);  
 
       // get the image data for the new canvas
       var imageData = ctx.getImageData( 0,0, imageCanvas.width, imageCanvas.height ); // get the underlying pixel data from the canvas  
-
       // reset the new canvas data with the dithered data
-      imageData.data.set(ditheredImageData)
-
+      imageData.data.set(q.reduce(imageCanvas))
       // put the new image data on the canvas
       ctx.putImageData( imageData, 0,0 ); // paint the canvas with the new imageData
 
@@ -112,10 +105,8 @@ export default {
       var q = new RgbQuant(this.rgbQuantOptions);  
       // analyze histograms
       q.sample(imageCanvas);  
-
-      var pal = q.palette(true);
-
-      this.rgbQuantOptions.palette = pal
+      // assign the palette
+      this.rgbQuantOptions.palette = q.palette(true);
 
     }
     
