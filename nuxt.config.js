43174fe5f925506a39f02995e3333218e0e753d8
alexharris <alexharris6@gmail.com>
2019-11-01 16:28:17 -0700

    build to docs folder

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,3 +1,9 @@
+const routerBase = process.env.DEPLOY_ENV === 'GH_PAGES' ? {
+  router: {
+    base: '/ditherit-v2/'
+  }
+} : {}
+
 export default {
   mode: 'spa',
   /*
@@ -49,5 +55,11 @@ export default {
      ** You can extend webpack config here
      */
     extend(config, ctx) {}
-  }
+  },
+  generate: {
+    dir: 'docs'
+  },
+  routerBase
 }
+
+
