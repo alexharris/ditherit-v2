e59d112d549abe245db05975dd17f6368be8bee7
alex harris <alexharris6@gmail.com>
2026-04-20 18:04:38 -0700

    Update Fathom analytics to new script tag (XLUDNBHI)

--- nuxt.config.js
+++ nuxt.config.js
@@ -38,15 +38,16 @@ export default {
     script: [
       {
         hid: 'fathom',
-        innerHTML: `(function(f,a,t,h,o,m){a[h]=a[h]||function(){(a[h].q=a[h].q||[]).push(arguments)};o=f.createElement('script');m=f.getElementsByTagName('script')[0];o.async=1;o.src=t;o.id='fathom-script';m.parentNode.insertBefore(o,m)})(document,window,'//cdn.usefathom.com/tracker.js','fathom');fathom('set','siteId','AHDLJXNJ');fathom('trackPageview');`
+        src: 'https://cdn.usefathom.com/script.js',
+        'data-site': 'XLUDNBHI',
+        defer: true
       },
       {
         src: '//static.getclicky.com/js',
         async: true,
         'data-id': '101479686'
       }
-    ],
-    __dangerouslyDisableSanitizers: ['script']
+    ]
   },
   /*
    ** Customize the progress-bar color
